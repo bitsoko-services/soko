6b976a04b673a00817ff185acef6ebbed025de2b
bitsoko <bitsoko@bitsoko.io>
2017-02-24 23:12:24 +0300

    Update custom-script.js

--- js/custom-script.js
+++ js/custom-script.js
@@ -108,7 +108,13 @@ profileLoaded(JSON.parse(event.target.result));
            
  }
 
-    
+$('.sidebar-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );    
     
     
     
