ac81f09658d74d0e1a3365b0442bbcaf07be0592
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-03-11 14:05:09 +0300

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // SOKO Server-JavaScript Document
 try {
-    sokoVersion = 19;
+    sokoVersion = 20;
     sokoInstall = function (event) {}
     sokoNotificationClick = function (dat) {
         // 	      event.notification.close();
