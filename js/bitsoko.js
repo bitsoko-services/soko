4a46b2f39932925cd75f05d1962d5c99a23e7658
bitsoko <bitsoko@bitsoko.io>
2017-03-26 18:16:40 +0300

    Update bitsoko.js

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -482,7 +482,14 @@ function addStore() {
     }).then(function (e) {
         getObjectStore('data', 'readwrite').put(JSON.stringify(e.reqs), 'bitsoko-merchant-requests-' + id);
     });
-    doFetch({
+  
+	refreshBeacons();
+    refreshProducts();
+    refreshPromotions();
+}
+
+function refreshBeacons(){
+  doFetch({
         action: 'getBeacons',
         id: localStorage.getItem('soko-owner-id')
     }).then(function (e) {
@@ -492,8 +499,6 @@ function addStore() {
     }).catch(function (err) {
         beaconsUpdater();
     }); //addCustomer(e.customers); 
-    refreshProducts();
-    refreshPromotions();
 }
 
 function addTransaction(t) {
@@ -923,9 +928,9 @@ function beaconsUpdater() {
         $(".beacons-holda-connected").html('');
         $(".beacons-holda-available").html('');
         if (reqs.length > 0) {
-            var html = '<li class="collection-item avatar"><i class="mdi-action-settings-bluetooth cyan circle"></i>' + '<span class="collection-header">Connected Beacons</span><p>' + reqs.length + ' Found</p></li>';
+            var html = '<li class="collection-item avatar"><i class="mdi-action-settings-bluetooth green circle"></i>' + '<span class="collection-header">Connected Beacons</span><p>' + reqs.length + ' Found</p></li>';
             $(".beacons-holda-connected").append($.parseHTML(html));
-            var html = '<li class="collection-item avatar"><i class="mdi-action-settings-bluetooth cyan circle"></i>' + '<span class="collection-header">Available Beacons</span><p>' + reqs.length + ' Found</p></li>';
+            var html = '<li class="collection-item avatar"><i class="mdi-action-settings-bluetooth pink circle"></i>' + '<span class="collection-header">Available Beacons</span><p>' + reqs.length + ' Found</p></li>';
             $(".beacons-holda-available").append($.parseHTML(html));
         } else {
             var html = '<li class="collection-item avatar"><i class="mdi-action-settings-bluetooth cyan circle"></i>' + '<span class="collection-header">No Beacons Found</span><p>click here to add a beacon</p></li>';
@@ -935,10 +940,10 @@ function beaconsUpdater() {
         var st = JSON.parse(localStorage.getItem('soko-store-id-' + localStorage.getItem('soko-active-store')));
         for (var i = 0, st = st; i < reqs.length; ++i) {
             if (parseInt(reqs[i].service) == parseInt(localStorage.getItem('soko-active-store'))) {
-                var html = '<li class="collection-item">' + '<div class="row"><div class="col s5">' + '<p class="collections-title"><strong>#' + reqs[i].name + '</strong> Connected</p></div><div class="col s5"><div class="select-wrapper initialized"><span class="caret">▼</span><select class="initialized"><option selected="" value="' + st.id + '">' + st.name + '</option><option value="0">disabled</option></select></div></div></div></li>';
+                var html = '<li class="collection-item">' + '<div class="row"><div class="col s5">' + '<p class="collections-title"><strong>#' + reqs[i].name + '</strong> Connected</p></div><div class="col s5"><div class="select-wrapper initialized"><span class="caret">▼</span><select class="initialized"><option selected="" value="' + st.id + '" bid="' + reqs[i].id + '">' + st.name + '</option><option value="0" bid="' + reqs[i].id + '">disabled</option></select></div></div></div></li>';
                 $(".beacons-holda-connected").append($.parseHTML(html));
             } else if (parseInt(reqs[i].service) == parseInt('0')) {
-                var html = '<li class="collection-item">' + '<div class="row"><div class="col s5">' + '<p class="collections-title"><strong>#' + reqs[i].name + '</strong> Not Connected</p></div><div class="col s5"><div class="select-wrapper initialized"><span class="caret">▼</span><select class="initialized"><option value="0" selected="">disabled</option><option value="' + st.id + '">' + st.name + '</option></select></div></div></div></li>';
+                var html = '<li class="collection-item">' + '<div class="row"><div class="col s5">' + '<p class="collections-title"><strong>#' + reqs[i].name + '</strong> Not Connected</p></div><div class="col s5"><div class="select-wrapper initialized"><span class="caret">▼</span><select class="initialized"><option value="0" selected="" bid="' + reqs[i].id + '">disabled</option><option value="' + st.id + '" bid="' + reqs[i].id + '">' + st.name + '</option></select></div></div></div></li>';
                 $(".beacons-holda-available").append($.parseHTML(html));
             }
         }
@@ -1463,6 +1468,36 @@ function addProduct() {
         } else {
             console.log(e);
         }
+    });
+}
+
+function updateBeacon() {
+    var forEach = function (array, callback, scope) {
+        for (var i = 0; i < array.length; i++) {
+            callback.call(scope, i, array[i]); // passes back stuff we need
+        }
+    };
+    var myNodeList = document.querySelectorAll('.beacons input');
+    forEach(myNodeList, function (index, value) {
+         value.addEventListener("change", function(e){
+		  var value = this;
+        var val = $(value).attr('bid');
+	 
+    doFetch({
+        action: 'doSetBeacon',
+        id: val,
+        to: $(value).val()
+    }).then(function (e) {
+        if (e.status == 'ok') {
+		
+            Materialize.toast('beacon updated ..', 3000);
+            refreshBeacons();
+		
+        } else {
+            console.log(e);
+        }
+    });
+	 });
     });
 }
 /*
