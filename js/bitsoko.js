910415134ab9056a80e9326b096effc2d426ed68
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-04-02 13:26:24 +0300

    billing update

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -414,7 +414,7 @@ function refreshBills(month, year) {
         //$('.month').html(month_name);
         var billcharges = parseFloat(e.reqs.length * 0.167).toFixed(3);
         console.log("Biliing charges----------->>" + billcharges)
-        $('#serviceBillCharges').html(billcharges); =
+        $('#serviceBillCharges').html(billcharges);
         if (e.status == "ok") {
             console.log(e)
         }
