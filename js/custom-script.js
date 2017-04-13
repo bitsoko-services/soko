c31cc6352595b3ec684c4c96a99ca5b992e92004
bitsoko <bitsoko@bitsoko.io>
2017-04-13 21:05:46 +0300

    Update custom-script.js

--- js/custom-script.js
+++ js/custom-script.js
@@ -100,10 +100,31 @@ angular.module('sokoApp', [])
             $('.sidebar-collapse').sideNav('hide');
             $('body').attr('page', $(this).attr('page'));
             $('#content > .container > .' + $(this).attr('page')).css('display', 'block');
+		updateThisPage($(this).attr('page'));
         }
         $(this).toggleClass('active');
     });
 });
+
+function updateThisPage(pg) {
+	
+	switch (pg) {
+            case 'products':
+                productsUpdater();
+		break;
+            case 'storefront':
+                promoUpdater();
+		break;
+            case 'beacons':
+               beaconsUpdater();
+		break;
+            case 'billing':
+                billingUpdater();
+		break;
+            default:
+        }
+	
+}
 
 function reqProfile() {
     var reqProf = "b:m:p-" + randomString(20)
