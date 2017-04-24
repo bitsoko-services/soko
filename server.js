09fdfd30a52a90ae4feda42f73231eb34e651408
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-04-24 09:11:28 +0300

    update 76

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // SOKO Server-JavaScript Document
 try {
-    sokoVersion = 75;
+    sokoVersion = 76;
     sokoInstall = function (event) {}
     sokoNotificationClick = function (dat) {
         // 	      event.notification.close();
