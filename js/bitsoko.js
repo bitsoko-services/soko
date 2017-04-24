53defb4dcb1712c467751f3199ab31adc335361d
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-04-24 08:53:02 +0300

    image primary color as default theme color

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -1221,9 +1221,9 @@ function orderUpdater() {
             // var saleTime=moment(reqs[i].posted).fromNow();
             var html = '<div id="order-card" class="card horizontal"><div class="card-image"><img src="' + reqs[i].icon + '"></div> <div class="card-stacked">' +
                 '<div class="card-content"><div class="">New Order</div><div class="orders-' + reqs[i].id + '-items"></div> </div>' +
-                ' <div class="card-action"><a href="tel:' + reqs[i].phone + '">call</a><a href="#">bill</a></div>'+
-		' <div class="card-action"><a href="#">cancel</a><a href="#">complete</a></div>'+
-		'</div></div>';
+                ' <div class="card-action"><a href="tel:' + reqs[i].phone + '">call</a><a href="#">bill</a></div>' +
+                ' <div class="card-action"><a href="#">cancel</a><a href="#">complete</a></div>' +
+                '</div></div>';
             // var html = '<div class="card"><div class="card-image waves-effect waves-block waves-light">' + '<img class="activator" src="' + reqs[i].promoBanner + '" alt="user bg"></div><div class="card-content" style="padding: 0px 20px;">' + '<img src="' + reqs[i].promoLogo + '" alt="" class="circle responsive-img activator card-profile-image">' + '<a class="btn-floating activator btn-move-up waves-effect waves-light darken-2 right">' + '<svg class="activator" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 490.3 490.3" style="enable-background:new 0 0 490.3 490.3;width: 26px;margin-left: 8px;margin-top: 7px;" xml:space="preserve"><g xmlns="http://www.w3.org/2000/svg"><path d="M438.931,30.403c-40.4-40.5-106.1-40.5-146.5,0l-268.6,268.5c-2.1,2.1-3.4,4.8-3.8,7.7l-19.9,147.4 c-0.6,4.2,0.9,8.4,3.8,11.3c2.5,2.5,6,4,9.5,4c0.6,0,1.2,0,1.8-0.1l88.8-12c7.4-1,12.6-7.8,11.6-15.2c-1-7.4-7.8-12.6-15.2-11.6 l-71.2,9.6l13.9-102.8l108.2,108.2c2.5,2.5,6,4,9.5,4s7-1.4,9.5-4l268.6-268.5c19.6-19.6,30.4-45.6,30.4-73.3 S458.531,49.903,438.931,30.403z M297.631,63.403l45.1,45.1l-245.1,245.1l-45.1-45.1L297.631,63.403z M160.931,416.803l-44.1-44.1 l245.1-245.1l44.1,44.1L160.931,416.803z M424.831,152.403l-107.9-107.9c13.7-11.3,30.8-17.5,48.8-17.5c20.5,0,39.7,8,54.2,22.4 s22.4,33.7,22.4,54.2C442.331,121.703,436.131,138.703,424.831,152.403z" fill="#FFFFFF"></path></g></svg></a><p>' + reqs[i].promoName + '</p><p>' + reqs[i].promoDesc + '</p>' + '<p style="text-align: center;padding: 15px 20px;"><i style="float: left;" class="promo-state-icon mdi-notification-sync"> 0 shares</i>' + '<i class="promo-state-icon mdi-action-favorite"> 0 likes </i>' + '<i style="float: right;" class="promo-state-icon mdi-action-receipt"> 0 sales </i></p>' + '<label>offer subscribers</label><div class="divider" style="margin: 10px;"></div><div class="promo-' + reqs[i].id + '-subscribers"></div>' + '</div><div class="card-reveal">' + '<form class="col s12"> <div class="row"> <div class="input-field col s12"> <input id="newPromo-name" type="text" class="validate js-loc-button-notification-input" value="" value="" stitm="name" required> <label for="newPromo-name" class="">Name</label> </div></div><div class="row"> <div class="input-field col s12"> <input id="newPromo-desc" type="text" class="validate js-loc-button-notification-input" value="" stitm="msg" required> <label for="newPromo-desc" class="">Desc</label> </div></div><div class="row"> <div class="file-field input-field"> <div class="btn"><span>image</span> <input id="newPromo-image" type="file" stitm="customImage" required> </div></div></div><div class="row"> <div class="input-field col s6"> <input placeholder="" id="newPromo-discount" type="number" class="validate" min="0" max="90"> <label for="newPromo-discount" class="">% discount</label> </div><div class="input-field col s6"> <input placeholder="" id="newPromo-offers" type="number" class="validate" min="0"> <label for="newPromo-offers" class="">minimum buyers</label> </div></div><div class="row" style="height:200px;overflow:auto;"> <h6 style="text-align:center;">Add an item to this promotion</h6> <ul class="promo-add-new-promotion2"></ul> </div></form>' + '<div class="row" style="text-align: center;margin: 20px 0px;"> <a class="removePromo waves-effect waves-light btn" style="margin-bottom:10px;">remove promotion</a><br><a class="backBtnPromo waves-effect waves-light btn">back</a> </div>' + '</div></div>';
             $(".orders-holda").prepend($.parseHTML(html));
             // addOrderItems(reqs[i].id, reqs[i].items);
@@ -2003,6 +2003,10 @@ $('#makeCard').click(function () {
 
 });
 
+//scroll to top onclick add promo 
+$("#makeCard").on("click", function () {
+    $(window).scrollTop(0);
+});
 
 //Products Form Validation
 $('#submitProdForm').click(function (e) {
@@ -2091,15 +2095,100 @@ $(document).ready(function () {
     $(window).resize(checkSize);
 });
 
-//Function to the css rule
-function checkSize() {
-    if ($(".prodImgDskt").css("float") == "none") {
-        // your code here
-        $('.prodImgDskt').css({
-            "background-color": "red !important",
-            "width": "100% !important",
-            "height": "50% !important",
-            "background-size": "contain !important"
-        });
+//Default Store Theme Color
+(function ($, window, document, undefined) {
+
+    'use strict';
+
+    var defaultColor = 'primaryColor',
+        dataName = 'primary-color',
+        defaults = {
+            skip: 5,
+            callback: null
+        };
+
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, this.configure(options));
+        this._defaults = defaults;
+        this._name = defaultColor;
+        this.primary = {
+            rgb: '',
+            count: 0
+        };
+        this.init();
     }
-}
+
+    $.extend(Plugin.prototype, {
+        init: function () {
+            $(new Image()).on('load', $.proxy(this.onLoad, this)).prop('src', this.element.src || '');
+        },
+        configure: function (options) {
+            if (typeof options === 'function') {
+                return {
+                    callback: options
+                };
+            } else if (typeof options === 'object' || !options) {
+                return options;
+            }
+            return {};
+        },
+        getImageData: function () {
+            var canvas = document.createElement('canvas');
+            canvas.width = this.element.width;
+            canvas.height = this.element.height;
+            var context = canvas.getContext('2d');
+            context.drawImage(this.element, 0, 0);
+            return context.getImageData(0, 0, this.element.width, this.element.height);
+        },
+        isApproximateColor: function (color1, color2) {
+            if (!color1 || !color2) {
+                return false;
+            }
+            var c1 = color1.split(','),
+                c2 = color2.split(','),
+                r = c1[0] - c2[0],
+                g = c1[1] - c2[1],
+                b = c1[2] - c2[2],
+                l = Math.sqrt(r * r + g * g + b * b);
+            return l < 60;
+        },
+        onLoad: function () {
+            var image_data = this.getImageData(),
+                data = image_data.data,
+                pixel_length = data.length / 4,
+                colors = {};
+            for (var px = 0; px < pixel_length; px = px + this.settings.skip * 4) {
+                if (data[px + 3] < 255) {
+                    continue;
+                }
+
+                var rgb = [data[px], data[px + 1], data[px + 2]].join(',');
+                if (this.primary.rgb && this.isApproximateColor(this.primary.rgb, rgb)) {
+                    rgb = this.primary.rgb;
+                }
+                colors[rgb] = colors[rgb] || 0;
+                var count = ++colors[rgb];
+                if (count > this.primary.count) {
+                    this.primary.rgb = rgb;
+                    this.primary.count = count;
+                }
+            }
+
+            $.data(this.element, dataName, this.primary.rgb);
+
+            if (typeof this.settings.callback === 'function') {
+                this.settings.callback.call(this.element, this.primary.rgb);
+            }
+        }
+    });
+
+    $.fn[defaultColor] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + defaultColor)) {
+                $.data(this, 'plugin_' + defaultColor, new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
