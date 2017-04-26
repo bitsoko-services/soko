31343b7374af77c9216a20edad3e009481973f14
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-04-26 12:58:26 +0300

    phone verification update

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -1905,10 +1905,10 @@ $('.collection-item.avatar').on('touchend', function (e) {
 $('#deliveriesToggle').click(function (e) {
     e.preventDefault();
     $('#deliveriesToggle').sideNav('hide');
-    $('#confirmContact').modal('open');
-    $('#confirmContact').modal({
-        dismissible: false,
-    });
+    $('#mobileVerification').modal('open');
+    //    $('#mobileVerification').modal({
+    //        dismissible: false,
+    //    });
     var isValid = true;
     $('#submitPhoneNo').click(function () {
         phoneNo_ = $('#phoneNumber').val();
@@ -1933,14 +1933,34 @@ $('#deliveriesToggle').click(function (e) {
 });
 
 //Enable Payments
-$('#paymentsToggle').click(function () {
-    $('#deliveriesToggle').sideNav('hide');
-    $('#confirmContact').modal('open');
-    var value = document.getElementById("paymentsToggle").checked
-    doFetch({
-        action: 'togglePayments',
-        value: value
-    }).then(function (e) {});
+$('#paymentsToggle').click(function (e) {
+    e.preventDefault();
+    $('#paymentsToggle').sideNav('hide');
+    $('#mobileVerification').modal('open');
+    //    $('#mobileVerification').modal({
+    //        dismissible: false,
+    //    });
+    var isValid = true;
+    $('#submitPhoneNo').click(function () {
+        phoneNo_ = $('#phoneNumber').val();
+        if (phoneNo_ == '' || phoneNo_ == null) {
+            Materialize.toast('Ooops! Please enter phone number', 3000);
+            $('#phoneNumber').css({
+                "border-bottom": "1px solid red",
+                "background": ""
+            });
+        } else {
+
+            var value = document.getElementById("paymentsToggle").checked
+            doFetch({
+                action: 'togglePayments',
+                value: value
+            }).then(function (e) {});
+            $('#paymentsToggle').prop('checked', true);
+        }
+    });
+    if (isValid == false)
+        e.preventDefault();
 });
 
 //Enable Loyalty
@@ -2195,3 +2215,103 @@ $(document).ready(function () {
     };
 
 })(jQuery, window, document);
+
+
+
+
+//----------------------------------------------bits addMobiVeri--------------------------------------------------------------------------------------
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------updating addMobiVeri ---------------------------------------------------------------------------------
+function addMobiVeri() {
+    //----------------------------------------------append contact modal--------------------------------------------------------------------------------------
+    $(".MobileModal").append('<div class="modal-content">' +
+        ' <div class="row">' +
+        '<div class="input-field col s8">' +
+        '  <i class="mdi-hardware-phone-android prefix"></i>' +
+        '  <input id="inp-phone" type="number" class="validate" >' +
+        '  <label for="inp-phone" class="">enter phone number</label>' +
+        ' </div>' +
+        ' <div class="input-field col s4">' +
+        '  <div class="input-field col s12">' +
+        '    <button class="inp-phone btn cyan waves-effect waves-light" type="submit" name="action" disabled><i class="mdi-navigation-refresh"></i></button>' +
+        '  </div>' +
+        ' </div>' +
+        ' </div>' +
+        ' <div class="row">' +
+        ' <div class="input-field col s8">' +
+        '  <i class="mdi-action-lock-outline prefix"></i>' +
+        ' <input id="inp-code" type="number" class="validate" disabled>' +
+        '  <label for="inp-code" class="">confirmation code</label>' +
+        ' </div>' +
+        ' <div class="input-field col s4">' +
+        '  <div class="input-field col s12">' +
+        '    <button class="inp-code btn cyan waves-effect waves-light" type="submit" name="action" disabled><i class="mdi-navigation-refresh"></i></button>' +
+        '  </div>' +
+        '  </div>' +
+        ' </div>' +
+
+        '</div>' +
+        ' <div class="modal-footer blue-grey lighten-5">' +
+        ' <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>' +
+        '</div>');
+
+    var forEach = function (array, callback, scope) {
+        for (var i = 0; i < array.length; i++) {
+            callback.call(scope, i, array[i]); // passes back stuff we need
+        }
+    };
+    var myNodeList = document.querySelectorAll('input#inp-phone');
+    forEach(myNodeList, function (index, value) {
+        value.addEventListener("change", changedPhnNum);
+    });
+    var myNodeList = document.querySelectorAll('input#inp-code');
+    forEach(myNodeList, function (index, value) {
+        value.addEventListener("change", changedConfCode);
+    });
+}
+
+function changedPhnNum(t) {
+    console.log($(t.target).val());
+    var val = $(t.target).val();
+
+    doFetch({
+        action: 'doMobiVeri',
+        address: localStorage.getItem('bits-user-wallet'),
+        val: val
+    }).then(function (e) {
+        if (e.status == 'ok') {
+            $('#inp-phone').prop('disabled', true);
+            $('#inp-code').prop('disabled', false);
+            Materialize.toast('confirmation code sent', 5000);
+
+        } else {
+
+            $('#inp-phone').prop('disabled', false);
+            $('#inp-code').prop('disabled', true);
+            console.log(e);
+        }
+    });
+
+}
+
+function changedConfCode(t) {
+    console.log($(t.target).val());
+    var val = $(t.target).val();
+
+    doFetch({
+        action: 'doMobiVeriCode',
+        address: localStorage.getItem('bits-user-wallet'),
+        val: val
+    }).then(function (e) {
+        if (e.status == 'ok') {
+
+            $('#inp-phone').prop('disabled', false);
+            $('#inp-code').prop('disabled', true);
+            Materialize.toast('Phone Number added', 3000);
+
+        } else {
+            console.log(e);
+        }
+    });
+
+}
