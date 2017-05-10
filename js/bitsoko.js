9d4b6c13b1f99d679e8e2094fc12ea458da48b81
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-05-10 07:38:59 +0300

    persistent storage

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -532,3 +532,14 @@ function rewardPoints() {
         }
     })
 }
+
+function persistentFunc() {
+    if (navigator.storage && navigator.storage.persist)
+        navigator.storage.persisted().then(persistent => {
+            if (persistent)
+                console.log("Storage will not be cleared except by explicit user action");
+            else
+                console.log("Storage may be cleared by the UA under storage pressure.");
+        });
+}
+persistentFunc();
