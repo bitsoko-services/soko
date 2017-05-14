23ab0451aa7177b508686cedd888d6cc5033a35b
bitsoko <bitsoko@bitsoko.io>
2017-05-14 22:24:41 +0300

    Update bitsoko.js

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -516,6 +516,20 @@ function transferListener() {
 
 
 
+function deliveryListener() {
+    var forEach = function (array, callback, scope) {
+        for (var i = 0; i < array.length; i++) {
+            callback.call(scope, i, array[i]); // passes back stuff we need
+        }
+    };
+    var myNodeList = document.querySelectorAll('#delivery-members');
+    forEach(myNodeList, function (index, value) {
+        value.addEventListener("change", userNamesInput('delivery-members'));
+    });
+}
+
+
+
 
 
 //rewardPoints()
