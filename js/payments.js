54c5b53c345e4049e1270bc2791de8b270576bfa
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-05-18 02:20:18 +0300

    payments update

--- js/payments.js
+++ js/payments.js
@@ -1,27 +1,18 @@
 //Enable Payments
-$('#paymentsToggle').click(function (e) {
-    e.preventDefault();
-    $('#paymentsToggle').sideNav('hide');
-    $('#MobileModal').modal('open');
-    var isValid = true;
-    $('#submitPhoneNo').click(function () {
-        phoneNo_ = $('#inp-phone').val();
-        if (phoneNo_ == '' || phoneNo_ == null) {
-            Materialize.toast('Ooops! Please enter phone number', 3000);
-            $('#phoneNumber').css({
-                "border-bottom": "1px solid red",
-                "background": ""
-            });
-        } else {
-            $('#paymentsToggle').prop('checked', true);
-            Materialize.toast('Phone Number Verified', 3000);
-            var value = document.getElementById("paymentsToggle").checked
-            var phone_number = $('#inp-phone').val()
-            doFetch({
-                action: 'togglePayments',
-                value: value,
-                id: localStorage.getItem('soko-active-store')
-            }).then(function (e) {});
-        }
-    });
+$('#paymentsToggle').click(function () {
+    $('#deliveriesToggle').sideNav('hide');
+    $('#confirmContact').modal('open');
+    var value = document.getElementById("paymentsToggle").checked
+    doFetch({
+        action: 'togglePayments',
+        value: value
+    }).then(function (e) {});
+});
+
+$('#paymentsToggle').click(function mobifer() {
+    userNamesInput();
+    if ($('#editStore-Phone').val() == "") {
+        $('#editStoreModal').modal('open');
+        Materialize.toast('Please add your phone number', 3000);
+    }
 });
