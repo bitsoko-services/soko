9a06cf6dd87de9a24bde2c1bbe1513a8f990fd60
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-05-18 02:46:09 +0300

    payments update

--- js/payments.js
+++ js/payments.js
@@ -1,18 +1,23 @@
 //Enable Payments
 $('#paymentsToggle').click(function () {
-    $('#deliveriesToggle').sideNav('hide');
-    $('#confirmContact').modal('open');
-    var value = document.getElementById("paymentsToggle").checked
-    doFetch({
-        action: 'togglePayments',
-        value: value
-    }).then(function (e) {});
-});
-
-$('#paymentsToggle').click(function mobifer() {
-    userNamesInput();
-    if ($('#editStore-Phone').val() == "") {
+    deliveryListener();
+    if (JSON.parse(localStorage.getItem('soko-store-id-' + localStorage.getItem('soko-active-store'))).phone == "") {
+        $('#paymentsToggle').sideNav('hide');
         $('#editStoreModal').modal('open');
         Materialize.toast('Please add your phone number', 3000);
+    } else {
+        $('#deliveriesToggle').sideNav('hide');
+        $('#confirmContact').modal('open');
+        var value = document.getElementById("paymentsToggle").checked
+        doFetch({
+            action: 'togglePayments',
+            value: value
+        }).then(function (e) {
+            if (e.status == 'ok') {
+
+            } else {
+
+            }
+        });
     }
 });
