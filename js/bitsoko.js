b4f2d1072094e39c918c5e21dfafcd2ebeb66403
Gaseema Ndung'u <gaseema.n@gmail.com>
2017-05-25 14:21:11 +0300

    UI update

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -616,3 +616,16 @@ function persistentFunc() {
         });
 }
 persistentFunc();
+
+
+//Box Shadow On Specific Pages
+$(".pageBoxShadow").click(function () {
+    $('.navbar-fixed nav').css({
+        'box-shadow': '0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2)',
+    });
+});
+$(".clickPromo").click(function () {
+    $('.navbar-fixed nav').css({
+        'box-shadow': 'none',
+    });
+});
