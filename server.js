20422174e5345ef38c0427059a7c09157dcc6b8c
Gaseema <gaseema.n@gmail.com>
2017-07-03 22:49:10 +0300

    update 152

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // SOKO Server-JavaScript Document
 try {
-    sokoVersion = 151;
+    sokoVersion = 152;
     sokoInstall = function (event) {}
     sokoNotificationClick = function (dat) {
         // 	      event.notification.close();
