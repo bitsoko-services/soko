86b16cb7ed9ee3181c272e27ad3d576b84fdabcc
Gaseema <gaseema.n@gmail.com>
2017-07-10 10:46:43 +0300

    distance btwn order and shop update

--- js/sales.js
+++ js/sales.js
@@ -259,11 +259,11 @@ function orderUpdater() {
                 }
             }
             if (orderCrdRate == 0) {
-                console.log(Math.ceil(getDistanceFromLatLonInKm(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY)));
-                $("#oderDeliveryRate_" + reqs[i].id).text(Math.ceil(getDistanceFromLatLonInKm(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY)))
+                console.log(Math.ceil(getDistance(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY)));
+                $("#oderDeliveryRate_" + reqs[i].id).text(Math.ceil(getDistance(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY)))
             } else {
-                console.log(Math.ceil(getDistanceFromLatLonInKm(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY) * orderCrdRate));
-                $("#oderDeliveryRate_" + reqs[i].id).text(Math.ceil(getDistanceFromLatLonInKm(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY) * orderCrdRate))
+                console.log(Math.ceil(getDistance(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY) * orderCrdRate));
+                $("#oderDeliveryRate_" + reqs[i].id).text(Math.ceil(getDistance(orderCrdLocationX, orderCrdLocationY, shopLocationX, shopLocationY) * orderCrdRate))
             }
         }
 
@@ -413,4 +413,23 @@ function getDateHours(date_from_server) {
             return minutes + ' min ago'
         }
     }
+}
+
+
+//Get distance between the shop and the order
+function getDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
 }
