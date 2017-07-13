49791d0acd5867a5d4ce61e0a4943e7d7d381147
bitsoko <bitsoko@bitsoko.io>
2017-07-13 11:12:24 +0300

    Update promo.js

--- js/promo.js
+++ js/promo.js
@@ -316,23 +316,13 @@ function updateProm(t) {
     var name = $(t.target).attr('pritm');
     var val = $(t.target).val();
     var switchID = $(t.target).attr('id')
-    var value = document.getElementById(switchID).checked
+    
     //console.log(value)
     if (name == "public") {
-        doFetch({
-            action: 'doEditPromo',
-            id: $(t.target).attr('fid'),
-            prop: name,
-            val: value
-        }).then(function (e) {
-            if (e.status == 'ok') {
-                Materialize.toast('modified ' + name + '..', 3000);
-            } else {
-
-                Materialize.toast('please try again..', 2000);
-        });
-
-    } else if (name == "customImage") {
+       var val = document.getElementById(switchID).checked
+    } 
+    
+    if (name == "customImage") {
             
             var files = t.target.files;
         var file = files[0];
