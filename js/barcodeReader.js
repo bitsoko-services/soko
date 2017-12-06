322267a38d5388667b41ee46cec2133178dedfb2
Gaseema <gaseema.n@gmail.com>
2017-12-06 18:08:43 +0300

    added quagga js

---
+++ js/barcodeReader.js
@@ -0,0 +1,29 @@
+$(".barCodeOpn").click(function () {
+    $('#barCodeReader').modal({
+        ready: function () {
+            Quagga.init({
+                inputStream: {
+                    name: "Live",
+                    type: "LiveStream",
+                    target: document.querySelector('#quagaLauncher') // Or '#yourElement' (optional)
+                },
+                decoder: {
+                    readers: ["code_128_reader"]
+                }
+            }, function (err) {
+                if (err) {
+                    console.log(err);
+                    return
+                }
+                console.log("Initialization finished. Ready to start");
+                Quagga.start();
+            })
+        },
+        complete: function () {
+            Quagga.stop()
+        }
+    }).modal('open');
+});
+$(".checkOutQuagga").click(function () {
+    $("#barCodeReader").modal("close")
+})
