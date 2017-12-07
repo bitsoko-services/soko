f78bb5779d458e121f17f689890e028f28d017e4
bitsoko <bitsoko@bitsoko.io>
2017-12-07 23:01:05 +0300

    Update barcodeReader.js

--- js/barcodeReader.js
+++ js/barcodeReader.js
@@ -1,10 +1,48 @@
+function addScannedToCart(prd){
+console.log(prd)
+}
+var newCart=[];
+
+
 $(".barCodeOpn").click(function () {
     $('#barCodeReader').modal({
         ready: function () {
              Quagga.onDetected(function(data){
-
-              console.log(data.codeResult.code) 
+             var bCode=data.codeResult.code;    
+               // we have detected a code, since indexdb are async we have to pause quagga first and test if the 
+               // scanned code is available in the list of products
+  Quagga.stop();
+        getObjectStore('data', 'readwrite').get('soko-store-' + localStorage.getItem('soko-active-store') + '-products').onsuccess = function (event) {
+var sts=$.parseJSON(event.target.result);
+            
+            
+            var cpt=false;
+            for(var j in sts ){
+                
+            if(sts[j].barCode==bCode){
+               cpt=true;
+               // we have found a matching code so we add it to the cart and continue scanning
+                
+                
+            addScannedToCart(sts[j]) 
+                alert('added '+sts[j].name+' to cart')
+               
+               }
+                if(!cpt){
+             // we have not found a matching code so we continue the scanning
+  Quagga.start();   
+                }
+            }
+        }  
+                 
+                 
+                 
+                 
+              console.log() 
               alert(data.codeResult.code) 
+              
+                 
+                 
             })
 
 
