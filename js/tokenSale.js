d75233a763fe3647f989320d114b55ec5aa4b5bf
Gaseema <gaseema.n@gmail.com>
2018-01-30 13:22:28 +0300

    Token Sale update

--- js/tokenSale.js
+++ js/tokenSale.js
@@ -2,4 +2,20 @@
 $(document).on("click", "#tokenSale", function () {
     $('#content > .container > div').css('display', 'none');
     $('#content > .container > .tokenSale').css('display', 'block');
+});
+
+//Activate / Deactive Token Sale
+$("#tkSaleSet").click(function () {
+    var mngrState = $("#tkSaleSet").prop("checked");
+    doFetch({
+        action: 'tokenize',
+        store: localStorage.getItem('soko-active-store'),
+        state: mngrState
+    }).then(function (e) {
+        if (e.status == 'ok') {
+            Materialize.toast('Activated successfully', 3000);
+        } else {
+            Materialize.toast('Ooops! Try again later', 3000);
+        }
+    });
 })
