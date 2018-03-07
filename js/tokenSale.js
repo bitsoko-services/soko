158590ec9c06ce0da014e8a646e26ee9bfc416b9
gaseema-n <gaseema@bitsoko.io>
2018-03-07 13:46:08 +0300

    token Sale update

--- js/tokenSale.js
+++ js/tokenSale.js
@@ -8,40 +8,40 @@ $(document).on("click", "#tokenSale", function () {
 //Buy Store Tokens
 $("#calcTokenVal").click(function () {
     var tokenValue = $("#tokenVal").val();
-    transferTokenValue("0x7D1Ce470c95DbF3DF8a3E87DCEC63c98E567d481", "0xb72627650f1149ea5e54834b2f468e5d430e67bf"), parseFloat(tokenValue), allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate).then(function (r) {
-    console.log("This is the TRID" + r);
-    if (tokenValue == "") {
-        M.toast({
-            html: 'Ooops! Please input amount',
-            displayLength: 3000
-        })
-    } else {
-        doFetch({
-            action: "creditStore",
-            contract: "0xb72627650f1149ea5e54834b2f468e5d430e67bf",
-            amount: tokenValue / (baseX * allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate),
-            rate: baseX * allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate,
-            user: localStorage.getItem("soko-owner-id"),
-            baseCd: baseCd,
-            shop: localStorage.getItem("soko-active-store"),
-            tran: r
-        }).then(function (e) {
-            if (e.status == 'ok') {
-                $(".prodCatToast").remove();
-                M.toast({
-                    html: 'Product category changed successfully',
-                    classes: 'prodCatToast',
-                    displayLength: 3000
-                })
-            } else {
-                M.toast({
-                    html: 'Error! Please try later',
-                    displayLength: 3000
-                })
-            }
-        });
-    }
-});
+    transferTokenValue("0x7D1Ce470c95DbF3DF8a3E87DCEC63c98E567d481", "0xb72627650f1149ea5e54834b2f468e5d430e67bf", parseFloat(tokenValue), allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate).then(function (r) {
+        console.log("This is the TRID" + r);
+        if (tokenValue == "") {
+            M.toast({
+                html: 'Ooops! Please input amount',
+                displayLength: 3000
+            })
+        } else {
+            doFetch({
+                action: "creditStore",
+                contract: "0xb72627650f1149ea5e54834b2f468e5d430e67bf",
+                amount: tokenValue / (baseX * allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate),
+                rate: baseX * allTokens["0xb72627650f1149ea5e54834b2f468e5d430e67bf"].rate,
+                user: localStorage.getItem("soko-owner-id"),
+                baseCd: baseCd,
+                shop: localStorage.getItem("soko-active-store"),
+                tran: r
+            }).then(function (e) {
+                if (e.status == 'ok') {
+                    $(".prodCatToast").remove();
+                    M.toast({
+                        html: 'Product category changed successfully',
+                        classes: 'prodCatToast',
+                        displayLength: 3000
+                    })
+                } else {
+                    M.toast({
+                        html: 'Error! Please try later',
+                        displayLength: 3000
+                    })
+                }
+            });
+        }
+    });
 });
 
 //Activate / Deactive Token Sale
