11b91224b01a5d8c421f4eea6b8ac07d6918a5be
bitsoko <bitsoko@bitsoko.io>
2018-05-29 14:40:33 +0300

    Update bitsoko.js

--- js/bitsoko.js
+++ js/bitsoko.js
@@ -19,6 +19,49 @@ stimeline = function () {
 //  Object.observe(stimeline, timelineObserver);
 flag = false;
 
+// Play oscillators at certain frequency and for a certain time
+var playNote = function (frequency, startTime, duration) {
+    var osc1 = context.createOscillator(),
+        osc2 = context.createOscillator(),
+        volume = context.createGain();
+ 
+    // Set oscillator wave type
+    osc1.type = 'triangle';
+    osc2.type = 'triangle';
+ 
+    volume.gain.value = 0.1;    
+ 
+    // Set up node routing
+    osc1.connect(volume);
+    osc2.connect(volume);
+    volume.connect(context.destination);
+ 
+    // Detune oscillators for chorus effect
+    osc1.frequency.value = frequency + 1;
+    osc2.frequency.value = frequency - 2;
+ 
+    // Fade out
+    volume.gain.setValueAtTime(0.1, startTime + duration - 0.05);
+    volume.gain.linearRampToValueAtTime(0, startTime + duration);
+ 
+    // Start oscillators
+    osc1.start(startTime);
+    osc2.start(startTime);
+ 
+    // Stop oscillators
+    osc1.stop(startTime + duration);
+    osc2.stop(startTime + duration);
+};
+
+var playPendingOrders = function () {
+    // Play a 'B' now that lasts for 0.116 seconds
+    playNote(493.883, context.currentTime, 0.116);
+ 
+    // Play an 'E' just as the previous note finishes, that lasts for 0.232 seconds
+    playNote(659.255, context.currentTime + 0.116, 0.232);
+};
+
+
 function profileLoaded(p) {
     $('.profile-image').attr('src', p.image);
     //p.ownerid=1;
