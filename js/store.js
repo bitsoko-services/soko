6a7b03e007c5d92c8cb158c95bf879f92e6b6def
gaseema-n <gaseema@bitsoko.io>
2018-05-29 13:53:26 +0300

    bug fixes

--- js/store.js
+++ js/store.js
@@ -281,7 +281,12 @@ function userNoPromp() {
 }
 
 function verifyNo() {
-    var checkPhoneNo = JSON.parse(localStorage.getItem('soko-store-id-' + localStorage.getItem('soko-active-store') + '')).phone
+    try {
+        var checkPhoneNo = JSON.parse(localStorage.getItem('soko-store-id-' + localStorage.getItem('soko-active-store') + '')).phone
+    } catch (err) {
+        console.log(err)
+        var checkPhoneNo = null
+    }
     if (checkPhoneNo == "") {
         userNoPromp()
     } else if (checkPhoneNo == null) {
