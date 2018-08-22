2e64eacf8e5a0a4de067f948063b08236f658415
gaseema-n <gaseema@bitsoko.io>
2018-08-22 17:09:26 +0300

    added packckckaging page

--- js/packaging.js
+++ js/packaging.js
@@ -1,5 +1,33 @@
 function showPackagingPage() {
     $('#content > .container > div').css('display', 'none');
-    $('#content > .container > .packaging').css('display', 'block');
-    $(".activePage").html("Packaging")
+    $(".activePage").html("Packaging");
+    setTimeout(function () {
+        $('.packaging').css('display', 'block');
+    }, 300)
+}
+
+function packagingData() {
+    var packagingData = [];
+    packagingData["smallPaperBag"] = $("#smallPaperbag").val();
+    packagingData["mediumPaperbag"] = $("#mediumPaperbag").val();
+    packagingData["largePaperbag"] = $("#largePaperbag").val();
+    doFetch({
+        action: 'requestPack',
+        id: localStorage.getItem('soko-active-store'),
+        items: packagingData
+    }).then(function (e) {
+        if (e.status == "ok") {
+            M.toast({
+                html: "Request sent successfully"
+            })
+        } else {
+            M.toast({
+                html: "Error! Try again later"
+            })
+        }
+    }).catch(function (err) {
+        M.toast({
+            html: "Error! Try again later"
+        })
+    });
 }
