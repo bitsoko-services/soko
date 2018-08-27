37430132f1688e20c2f9231cbe704a5180ead7f4
gaseema-n <gaseema@bitsoko.io>
2018-08-27 14:22:03 +0300

    packaging update

--- js/packaging.js
+++ js/packaging.js
@@ -37,26 +37,55 @@ function packagingData() {
         })
     }
 }
-$(document).on("keyup", ".packInput input", function (e) {
+
+function packagingTotalCost() {
     var smallPack = $("#smallPaperbag").val() * 10
     var mediumPack = $("#mediumPaperbag").val() * 20
     var largePack = $("#largePaperbag").val() * 40
+    if ($("#smallPaperbag").prop("disabled") == true) {
+        smallPack = 0
+    }
+    if ($("#mediumPaperbag").prop("disabled") == true) {
+        mediumPack = 0
+    }
+    if ($("#largePaperbag").prop("disabled") == true) {
+        largePack = 0
+    }
     var totalCost = smallPack + mediumPack + largePack
-    console.log(totalCost)
+    console.log("small" + smallPack)
+    console.log("mediumPack" + mediumPack)
+    console.log("largePack" + largePack)
     $("#packPrice").html(totalCost)
+}
+$(document).on("keyup", ".packInput input", function (e) {
+    packagingTotalCost()
 })
 $(document).on("click touchstart", ".packPlus", function () {
     var input = $(this).siblings("input");
     if ($(this).siblings(".packMinus").hasClass("disabled") == true) {
         $(this).siblings(".packMinus").removeClass("disabled");
     }
-    $(this).siblings("input").val(parseInt(input.val())+ 1);
+    $(this).siblings("input").val(parseInt(input.val()) + 1);
+    packagingTotalCost()
 })
 $(document).on("click touchstart", ".packMinus", function () {
     var input = $(this).siblings("input")
-    if (input.val() <= 2) {
+    if (input.val() <= 1) {
         $(this).addClass("disabled");
     } else {
         $(this).siblings("input").val(parseInt(input.val() - 1));
+        packagingTotalCost()
+    }
+});
+$(document).on("click touchstart", ".packCheckbox", function () {
+    var checked = $(this).prop("checked");
+    if (checked == true) {
+        $(this).siblings(".packCounter").find("button").attr("disabled", false);
+        $(this).siblings(".packCounter").find("input").attr("disabled", false);
+        $(this).siblings(".packCounter").find("button").removeClass("disabled");
+    } else {
+        $(this).siblings(".packCounter").find("button").attr("disabled", true);
+        $(this).siblings(".packCounter").find("input").attr("disabled", true);
+        $(this).siblings(".packCounter").find("button").addClass("disabled");
     }
 })
