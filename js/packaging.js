c103a7b40c066e2a205d64cc738095ad76bfbae0
gaseema.n <gaseema@bitsoko.io>
2018-10-15 08:57:29 +0300

    added wrapping bags in packaging

--- js/packaging.js
+++ js/packaging.js
@@ -1,98 +1,126 @@
 function showPackagingPage() {
     $('#content > .container > div').css('display', 'none');
     $(".activePage").html("Packaging");
-    setTimeout(function () {
+    setTimeout(function() {
         $('.packaging').css('display', 'block');
     }, 300)
 }
 
 function packagingDataArray() {
-     var packagingDataArray = [];
-    if ($("#smallPaperbag").prop("disabled") == false) {
-        packagingDataArray["small"] = $("#smallPaperbag").val();
-    }
-    if ($("#mediumPaperbag").prop("disabled") == false) {
-        packagingDataArray["medium"] = $("#mediumPaperbag").val();
-    }
-    if ($("#largePaperbag").prop("disabled") == false) {
-        packagingDataArray["large"] = $("#largePaperbag").val();
-    }
-    return packagingDataArray;
+    promise1 = new Promise(function(resolve, reject) {
+        var packagingDataArray = {};
+        if (packagingType == "paperBag") {
+            if ($("#smallPaperbag").prop("disabled") == false) {
+                packagingDataArray["small"] = $("#smallPaperbag").val();
+            } else {
+                packagingDataArray["small"] = "0"
+            }
+            if ($("#mediumPaperbag").prop("disabled") == false) {
+                packagingDataArray["medium"] = $("#mediumPaperbag").val();
+            } else {
+                packagingDataArray["medium"] = "0"
+            }
+            if ($("#largePaperbag").prop("disabled") == false) {
+                packagingDataArray["large"] = $("#largePaperbag").val();
+            } else {
+                packagingDataArray["large"] = "0"
+            }
+            resolve(packagingDataArray);
+        } else {
+            resolve($('#wrappingrBag').val());
+        }
+    });
 }
+$(document).on("click touchstart",".packagingCheckout", function(){
+    activePackaging = $(this).attr('packagingType');
+    packagingDataArray()
+    packagingData()
+})
 
 function packagingData() {
-   
+    if (packagingType == "paperBag") {} else {}
 
-    if ($('#packPrice').html().replace(/[^0-9\.]+/g, '') <= shopBalance) {
-        doFetch({
-            action: 'requestPack',
-            id: localStorage.getItem('soko-active-store'),
-            items: packagingDataArray(),
-            type: "paperbags"
-        }).then(function (e) {
-            if (e.status == "ok") {
-                M.toast({
-                    html: "Request sent successfully"
-                })
-            } else {
-                M.toast({
-                    html: "Error! Try again later"
-                })
-            }
-        }).catch(function (err) {
-            M.toast({
-                html: "Error! Try again later"
-            })
-        });
-    } else {
-        //creditTopup = $("#packPrice").html();
-        getInsufficientFundsOrderbook($("#packPrice").html()).then(function(r){
-
+    promise1.then(function(value) {
+        if ($('.packPrice').html().replace(/[^0-9\.]+/g, '') <= shopBalance) {
             doFetch({
-            action: 'requestPack',
-            id: localStorage.getItem('soko-active-store'),
-            items: packagingDataArray(),
-            type: "paperbags"
-        }).then(function (e) {
-            if (e.status == "ok") {
-                M.toast({
-                    html: "Request sent successfully"
-                })
-            } else {
+                action: 'requestPack',
+                id: localStorage.getItem('soko-active-store'),
+                items: value,
+                type: packagingType
+            }).then(function(e) {
+                if (e.status == "ok") {
+                    M.toast({
+                        html: "Request sent successfully"
+                    })
+                } else {
+                    M.toast({
+                        html: "Error! Try again later"
+                    })
+                }
+            }).catch(function(err) {
                 M.toast({
                     html: "Error! Try again later"
                 })
-            }
-        }).catch(function (err) {
-            M.toast({
-                html: "Error! Try again later"
-            })
-        });
+            });
+        } else {
+            //creditTopup = $(".packPrice").html();
+            getInsufficientFundsOrderbook($(".packPrice").html()).then(function(r) {
 
-        });
-    }
+                doFetch({
+                    action: 'requestPack',
+                    id: localStorage.getItem('soko-active-store'),
+                    items: value,
+                    type: "paperbags"
+                }).then(function(e) {
+                    if (e.status == "ok") {
+                        M.toast({
+                            html: "Request sent successfully"
+                        })
+                    } else {
+                        M.toast({
+                            html: "Error! Try again later"
+                        })
+                    }
+                }).catch(function(err) {
+                    M.toast({
+                        html: "Error! Try again later"
+                    })
+                });
+
+            });
+        }
+    });
 }
 
 function packagingTotalCost() {
+    var packagingType = $(this).attr('packagingType');
     var smallPack = $("#smallPaperbag").val() * 10
     var mediumPack = $("#mediumPaperbag").val() * 20
     var largePack = $("#largePaperbag").val() * 40
-    if ($("#smallPaperbag").prop("disabled") == true) {
-        smallPack = 0
+    var wrappingrBag = $("#wrappingrBag").val() * 5
+
+    if (packagingType == 'paperBag') {
+        if ($("#smallPaperbag").prop("disabled") == true) {
+            smallPack = 0
+        }
+        if ($("#mediumPaperbag").prop("disabled") == true) {
+            mediumPack = 0
+        }
+        if ($("#largePaperbag").prop("disabled") == true) {
+            largePack = 0
+        }
+        var totalCost = smallPack + mediumPack + largePack
+        $(".packPrice").html(totalCost + " " + baseCd)
+    } else {
+        var totalCost = wrappingrBag
+        $(".packPrice").html(totalCost + " " + baseCd)
     }
-    if ($("#mediumPaperbag").prop("disabled") == true) {
-        mediumPack = 0
-    }
-    if ($("#largePaperbag").prop("disabled") == true) {
-        largePack = 0
-    }
-    var totalCost = smallPack + mediumPack + largePack
-    $("#packPrice").html(totalCost + " " + baseCd)
+
 }
-$(document).on("keyup", ".packInput input", function (e) {
+$(document).on("keyup", ".packInput input", function(e) {
     packagingTotalCost()
 })
-$(document).on("click touchstart", ".packPlus", function () {
+$(document).on("click touchstart", ".packPlus", function() {
     var input = $(this).siblings("input");
     if ($(this).siblings(".packMinus").hasClass("disabled") == true) {
         if (input.val() > 49) {
@@ -102,8 +130,8 @@ $(document).on("click touchstart", ".packPlus", function () {
     $(this).siblings("input").val(parseInt(input.val()) + 1);
     packagingTotalCost()
 })
-$(document).on("click touchstart", ".packMinus", function () {
-    var input = $(this).siblings("input")
+$(document).on("click touchstart", ".packMinus", function() {
+    var input = $(this).siblings("input");
     if (input.val() < 51) {
         $(this).addClass("disabled");
     } else {
@@ -111,8 +139,9 @@ $(document).on("click touchstart", ".packMinus", function () {
         packagingTotalCost()
     }
 });
-$(document).on("click touchstart", ".packCheckbox", function () {
+$(document).on("click touchstart", ".packCheckbox", function() {
     var checked = $(this).prop("checked");
+    packagingType = $(this).attr('packagingType');
     if (checked == true) {
         $(this).siblings(".packCounter").find("button").attr("disabled", false);
         $(this).siblings(".packCounter").find("input").attr("disabled", false);
